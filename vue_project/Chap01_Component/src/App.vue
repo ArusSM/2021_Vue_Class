a23d01490f7bedfe5e53a2be1042d3c404134187
arussm <kimjng10@naver.com>
2021-04-02 15:48:53 +0900

    4월 2일

--- vue_project/Chap01_Component/src/App.vue
+++ vue_project/Chap01_Component/src/App.vue
@@ -1,32 +1,34 @@
 <template>
-  <div>
-    <img alt="Vue logo" src="./assets/logo.png">
+  <div class="card-body">
+    <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
     <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
     <h1>Vue Component</h1>
-    <Currency/>
+    <A02Binding></A02Binding>
   </div>  
 </template>
 
 <script>
 // import HelloWorld from './components/HelloWorld.vue'
-  import Currency from './components/A01.Currency'
+  // import Currency from './components/A01.Currency'
+  import A02Binding from './components/02.Binding.vue'
 
 export default {
   name: 'App',
   components: {
     // HelloWorld
-    Currency
+    A02Binding
+    // Currency
   }
 }
 </script>
 
 <style>
-#app {
+/* #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
-}
+} */
 </style>
