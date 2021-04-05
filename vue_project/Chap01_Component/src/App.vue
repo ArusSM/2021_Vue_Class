69748c8ae4551408ce9f49ab964f52540ecf397e
arussm <kimjng10@naver.com>
2021-04-05 12:56:23 +0900

    바인드 완성 다른 컴포넌트 넣어둠

--- vue_project/Chap01_Component/src/App.vue
+++ vue_project/Chap01_Component/src/App.vue
@@ -4,19 +4,28 @@
     <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
     <h1>Vue Component</h1>
     <A02Binding></A02Binding>
+    <A03Binding></A03Binding>
+    <A04Binding></A04Binding>
+    <A05Binding></A05Binding>
   </div>  
 </template>
 
 <script>
 // import HelloWorld from './components/HelloWorld.vue'
   // import Currency from './components/A01.Currency'
-  import A02Binding from './components/02.Binding.vue'
+  import A02Binding from './components/A02.Binding.vue'
+  import A03Binding from './components/A03.Repeater.vue'
+  import A04Binding from './components/A04.LifeCycle.vue'
+  import A05Binding from './components/A05.Event.vue'
 
 export default {
   name: 'App',
   components: {
     // HelloWorld
-    A02Binding
+    A02Binding,
+    A03Binding,
+    A04Binding,
+    A05Binding
     // Currency
   }
 }
---
+++ vue_project/Chap01_Component/src/components/A02.Binding.vue
@@ -0,0 +1,93 @@
+<template>
+ <div class="card-body">
+        <h3>{{title}}</h3>
+        <br>
+        <div>
+            v-text: <span v-text="text"></span><br>
+            v-html: <span v-html="text"></span><br>
+            v-once: <span v-once>{{text}}</span><br>
+            <!-- v-pre: <span ></span><br> -->
+            <br>
+            <button v-on:click="changeText('Nolbu')">Nolbu</button>
+            &nbsp;&nbsp;
+            <button @click="changeText('<b>Heungbu</b>')">Heungbu</button>
+            <br>
+            <br>
+            {{num}} <br>
+            <input type="text" class="form-control" v-model="num">
+            <input type="text" v-bind:class="formControl" v-model="num">
+            <input type="text" :class="formControl" v-model="num">
+            <br>
+            
+            <input type="checkbox" value="apple" v-model="fruit">사과, 
+            <input type="checkbox" value="banana" v-model="fruit">바나나, 
+            <input type="checkbox" value="melon" v-model="fruit">멜론<br>
+            <br>
+            <div> 당신이 선택한 과일은.. {{fruit}}</div>
+            <div><span v-for="(item, i) in fruit" :key="item">{{item}} {{i !== fruit.length-1 ? ',' : ""}}</span></div>
+            <div><span></span></div>
+        <br>
+        by computed: {{one}} <br>
+        by methods: {{two()}}
+        <br>
+
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return{
+            title : '2.binding',
+            text : '<b>Hello World</b>',
+            num: 10,
+            formControl : 'form-control',
+            fruit: []
+        }
+    },
+    methods : {
+        changeText: function(value) {
+            this.text = value;
+        },
+        two: function() {
+            console.log('methods');
+            let total = 0;
+            let x = Number(this.num);
+
+            if(isNaN(x)) {
+                total = 0;
+            }
+
+            for(let i = 0; i <= x; i ++) {
+                total += i;
+            }
+            return total;
+        }
+    },
+    computed: {
+        one: function() {
+            console.log('computed');
+            let total = 0;
+            let x = Number(this.num);
+
+            if(isNaN(x)) {
+                total = 0;
+            }
+
+            for(let i = 0; i <= x; i ++) {
+                total += i;
+            }
+            return total;
+        },
+    }
+}
+</script>
+
+<style scoped>
+button {
+    background-color: aqua;
+}
+</style>
+
+
